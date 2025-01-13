9be0f675b038a920cfd85631e6a4b9c17a1e9a9c
Yiduo Wang <yiduo@rice.edu>
2025-01-13 16:40:51 -0600

    update animation

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -1,5 +1,5 @@
 let all_paragraphs =
-	'h1, h2, h3, h4, h5, h6, p, .md-typeset';
+	'h1, h2, h3, h4, h5, h6, p, .md-typeset blockquote, .md-typeset dl, .md-typeset figure, .md-typeset ol, .md-typeset pre, .md-typeset ul, scroll-down-indicator';
 
 // Function to fade in paragraphs
 function fadeInParagraphs() {
@@ -15,10 +15,7 @@ function addFadeOutOnScroll() {
 	window.addEventListener('scroll', () => {
 		paragraphs.forEach((paragraph) => {
 			const rect = paragraph.getBoundingClientRect();
-			if (
-				!paragraph.classList.contains('md-footer-meta') &&
-				(rect.bottom < 100 || rect.top > window.innerHeight - 100)
-			) {
+			if (rect.bottom < 100 || rect.top > window.innerHeight - 100) {
 				paragraph.classList.add('fade-out');
 			} else {
 				paragraph.classList.remove('fade-out');
